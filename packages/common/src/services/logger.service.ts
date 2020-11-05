567f99e8e9341ef72031879b0a07f6430c1b1c5a
viction <viction.dev@gmail.com>
2020-11-05 21:03:11 +0000

    Fix logger.service appending array to every log

--- packages/common/src/services/logger.service.ts
+++ packages/common/src/services/logger.service.ts
@@ -8,21 +8,21 @@ export class Logger {
         this.formattedName = id ? `${name}[${id}]` : name;
     }
 
-    log(message: any, ...messages: any[]) {
-        console.log(chalk.green(`[${this.timestamp()}]`), chalk.green(`[INFO] ${this.formattedName} -`), chalk.dim(message), messages.map(msg => chalk.dim(msg)));
+    log(message: any) {
+        console.log(chalk.green(`[${this.timestamp()}]`), chalk.green(`[INFO] ${this.formattedName} -`), chalk.dim(message));
     }
 
-    debug(message: any, ...messages: any[]) {
+    debug(message: any) {
         // TODO: Debug env
-        console.log(chalk.cyan(`[${this.timestamp()}]`), chalk.cyan(`[DEBUG] ${this.formattedName} -`), chalk.dim(message), messages.map(msg => chalk.dim(msg)));
+        console.log(chalk.cyan(`[${this.timestamp()}]`), chalk.cyan(`[DEBUG] ${this.formattedName} -`), chalk.dim(message));
     }
 
-    warn(message: any, ...messages: any[]) {
-        console.log(chalk.yellow(`[${this.timestamp()}]`), chalk.yellow(`[WARN] ${this.formattedName} -`), chalk.dim(message), messages.map(msg => chalk.dim(msg)));
+    warn(message: any) {
+        console.log(chalk.yellow(`[${this.timestamp()}]`), chalk.yellow(`[WARN] ${this.formattedName} -`), chalk.dim(message));
     }
 
-    error(message: any, ...messages: any[]) {
-        console.log(chalk.red(`[${this.timestamp()}]`), chalk.red(`[ERROR] ${this.formattedName} -`), chalk.dim(message), messages.map(msg => chalk.dim(msg)));
+    error(message: any) {
+        console.log(chalk.red(`[${this.timestamp()}]`), chalk.red(`[ERROR] ${this.formattedName} -`), chalk.dim(message));
     }
 
     private timestamp() {
