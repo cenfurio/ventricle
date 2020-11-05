41974f788e8edbea0f1b80a047bd7620e2f91bb1
paulhobbel <itsdizzy40@gmail.com>
2020-11-05 16:28:28 +0100

    Removed gitlab-ci and updated the yarn lock to function properly again

--- packages/common/src/decorators/module.decorator.ts
+++ packages/common/src/decorators/module.decorator.ts
@@ -1,4 +1,4 @@
 import {makeDecorator} from "../util";
-import {ModuleMetadata} from '../interfaces/module.interface';
+import {ModuleMetadata} from '../interfaces';
 
 export const Module = makeDecorator('Module', (module: ModuleMetadata) => module);
